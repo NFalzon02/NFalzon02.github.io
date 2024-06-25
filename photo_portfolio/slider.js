1665991fb5bcd5a8b866bfd8998b897ad0f59094
NFalzon1 <nicholas.falzon.d56570@mcast.edu.mt>
2024-06-25 09:21:25 +0200

    Removal of slider

--- photo_portfolio/slider.js
+++
@@ -1,8 +0,0 @@
-let currentSlide = 0;
-
-function moveSlide(direction) {
-    const slides = document.querySelectorAll('.slide');
-    const totalSlides = slides.length;
-    currentSlide = (currentSlide + direction + totalSlides) % totalSlides;
-    document.querySelector('.slider').style.transform = `translateX(-${currentSlide * 100}%)`;
-}
